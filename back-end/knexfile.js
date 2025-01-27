8ec89fcfb203b5c78bdc2a6723bbdedf4232c67d
Mohit kumar <itsmohit3005@gmail.com>
2025-01-27 20:27:19 +0530

    fix: update production database connection to use detailed configuration with SSL

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -85,7 +85,16 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_PRODUCTION,
+    connection: {
+      host: DATABASE_URL_PRODUCTION,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
