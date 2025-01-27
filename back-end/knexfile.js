9aba37d5b6e802cb9705e9eb4987c9639292649a
Mohit kumar <itsmohit3005@gmail.com>
2025-01-27 20:37:35 +0530

    refactor: simplify production database connection configuration by using a single connection string

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -85,16 +85,8 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: {
-      host: DATABASE_URL_PRODUCTION,
-      port: 5432,
-      user: USER_NAME,
-      password: DATABASE_PASSWORD,
-      database: DATABASE_NAME,
-      ssl: {
-        rejectUnauthorized: false, // Skip certificate validation
-      },
-    },
+    connection: DATABASE_URL_PRODUCTION,
+
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
