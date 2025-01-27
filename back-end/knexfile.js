da04b34cf3447175f1f1aea7555694034b2f14ee
Mohit kumar <itsmohit3005@gmail.com>
2025-01-27 17:14:13 +0530

    database update: db url changed from elephantsql to AWS RDS

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,13 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_PRODUCTION = "postgresql://postgres@localhost/postgres",
+  USER_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
   DEBUG,
 } = process.env;
 
@@ -19,49 +22,85 @@ module.exports = {
   development: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_DEVELOPMENT,
+    connection: {
+      host: DATABASE_URL_DEVELOPMENT,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    debug: false,
   },
   test: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_TEST,
+    connection: {
+      host: DATABASE_URL_TEST,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    debug: false,
   },
   preview: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL_PREVIEW,
+    connection: {
+      host: DATABASE_URL_PREVIEW,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    debug: false,
   },
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
+    connection: {
+      host: DATABASE_URL_PRODUCTION,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
+    debug: false,
   },
 };
